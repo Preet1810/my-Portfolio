d5b1acf7cf6aadb38c7d1ff9162240c67406c2eb
Preet1810 <preetpalsingh1239@gmail.com>
2022-10-28 10:42:36 +0530

    email

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const nodemailer=require('nodemailer');
+
+if (process.env.NODE_ENV!=="production") {
+    require('dotenv').config();
+}
+
+
+app.use(express.static(path.join(__dirname, 'public')));  //setting up public directory
+app.use(express.json()) //for json parsed data
+app.set('view engine', 'ejs');  //setting up ejs
+app.set('views', path.join(__dirname, 'views'));  //setting up views directory
+
+
 
 
 app.get('/', (req, res) => {
     res.render('main')
 })
 
-app.use(express.static(path.join(__dirname, 'public')));  //setting up public directory
+app.post('/', (req, res) => {
+    // console.log(req.body)
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));  //setting up views directory
+    const transporter=nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'preetpalsingh1239@gmail.com',
+            pass: process.env.APP_PASSWORD
+        }
+    })
+
+    const mailOptions={
+        from: req.body.email,
+        to: 'preetpalsingh1239@gmail.com',
+        subject: `Message from ${req.body.email}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            // console.log('Email sent: '+info.response);
+            res.send('success')
+        }
+    })
+})
+
+
 
 const port=3000;
 app.listen(port, () => {
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,7 @@
 // const canvas=document.getElementById('canvas'),
 //     context=canvas.getContext('2d');
 
+
 // make_base();
 
 // function make_base() {
@@ -8,3 +9,41 @@
 //     base_image.src='https://i.pinimg.com/originals/c9/f2/5c/c9f25cd1e26a0c7528480848ad9b8e69.gif';
 //     context.drawImage(base_image, 200, 100);
 // }
+
+const contactForm=document.querySelector('.contact-form');
+let Sendername=document.getElementById('name');
+let email=document.getElementById('email');
+let message=document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData={
+        name: Sendername.value,
+        email: email.value,
+        subject: 'YOUR SITE',
+        message: message.value
+
+    }
+
+    let xhr=new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload=function () {
+        console.log(xhr.responseText);
+        if (xhr.responseText=='success') {
+            alert('Email Sent');
+            Sendername.value='';
+            email.value='';
+            subject.value='';
+            message.value='';
+        } else {
+            alert('Something Went Wrong!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData))
+
+})
+
+
